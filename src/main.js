19d9c5124976abea8b32fc83e8ef82bcc9adc5ec
75584612587 <38595739+75584612587@users.noreply.github.com>
2021-02-15 12:22:35 +0800

    Add cname to static, updat jquery version, rename to lowercase imports

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './app'
 import router from './router'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
